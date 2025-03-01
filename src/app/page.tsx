a5edd11340ab699a00f60f3e62fca8137f96031e
CharlesMarlow <gordon.nadav@gmail.com>
2025-03-01 16:20:16 +0100

    WIP: turn off home page inifinite loop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,45 +5,57 @@ import { useMutation } from '@tanstack/react-query';
 import CreatePostCard from '@/components/post/CreatePostCard';
 import { sendUserToBackend } from './api/services/user';
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
+import { useKindeBrowserClient } from '@kinde-oss/kinde-auth-nextjs';
 import { useRouter } from 'next/navigation';
 import { User } from '@/lib/types';
 
 export default function Home() {
-  const router = useRouter();
-  const { isAuthenticated, getUser } = getKindeServerSession();
+  // const router = useRouter();
+  // const { isAuthenticated, user } = useKindeBrowserClient();
 
   // Mutation for sending user data to backend
-  const mutation = useMutation({
-    mutationFn: sendUserToBackend,
-  });
+  // const mutation = useMutation({
+  //   mutationFn: sendUserToBackend,
+  // });
 
-  useEffect(() => {
-    const fetchUser = async () => {
-      const isLoggedIn = await isAuthenticated();
-      if (!isLoggedIn) {
-        router.push('/api/auth/login');
-        return;
-      }
+  const fetchUser = async () => {
+    // const isLoggedIn = await isAuthenticated;
+    // if (!isLoggedIn) {
+    //   router.push('/api/auth/login');
+    //   return;
+    // }
 
-      const user = await getUser();
-      console.log('User:', user);
+    // console.log('User:', user);
+  };
 
-      if (user) {
-        const transformedUser: User = {
-          id: user.id,
-          email: user.email ?? '',
-          given_name: user.given_name ?? '',
-          picture: user.picture ?? '',
-          posts: [],
-          friends: [],
-        };
+  fetchUser();
 
-        mutation.mutate(transformedUser);
-      }
-    };
+  // useEffect(() => {
+  //   const fetchUser = async () => {
+  //     const isLoggedIn = await isAuthenticated;
+  //     if (!isLoggedIn) {
+  //       router.push('/api/auth/login');
+  //       return;
+  //     }
 
-    fetchUser();
-  }, [isAuthenticated, getUser, mutation, router]);
+  //     console.log('User:', user);
+
+  //     if (user) {
+  //       const transformedUser: User = {
+  //         id: user.id,
+  //         email: user.email ?? '',
+  //         given_name: user.given_name ?? '',
+  //         picture: user.picture ?? '',
+  //         posts: [],
+  //         friends: [],
+  //       };
+
+  //       mutation.mutate(transformedUser);
+  //     }
+  //   };
+
+    // fetchUser();
+  // }, [isAuthenticated, mutation, router]);
 
   return (
     <div className='items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] '>
